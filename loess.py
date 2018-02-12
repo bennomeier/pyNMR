b4a76db78fc015c3ae8ad849e764e19e3e620889
Benno Meier <b.meier@soton.ac.uk>
2018-02-12 17:00:07 +0000

    Substantial speed improvement to tricube loess

--- loess.py
+++ loess.py
@@ -5,11 +5,11 @@ def loessInternal( x, h, xp, yp , kernel = "Gaussian"):
         w = np.exp( - (x-xp)**2/(2*h**2) )/np.sqrt(2*np.pi*h**2)
     elif kernel == "TriCube":
         w =  (1 - np.abs((x - xp) / h)**3)**3
-        for i, xThis in enumerate(xp):
-            if np.abs(xThis - x) > h:
-                w[i] = 0
 
-        
+        w = np.asarray(w)
+        low_values_flags = np.abs(xp - x) > h  # Where values are low
+        w[low_values_flags] = 0  # All low values set to 0
+                
     b = np.sum(w*xp)*np.sum(w*yp) - np.sum(w)*np.sum(w*xp*yp)
 
     b /= np.sum(w*xp)**2 - np.sum(w)*np.sum(w*xp**2)
@@ -30,14 +30,14 @@ def loess(x,y,h, kernel = "Gaussian"):
     return np.array(out)
     
 
-def splitLoess(x, y, sigma, SPLIT):
+def splitLoess(x, y, sigma, SPLIT, kernel = "Gaussian"):
     x1 = x[:SPLIT]
     y1 = y[:SPLIT]
     
     x2 = x[SPLIT:]
     y2 = y[SPLIT:]
     
-    y1Loess = loess(x1, y1, sigma)
-    y2Loess = loess(x2, y2, sigma)
+    y1Loess = loess(x1, y1, sigma, kernel = kernel)
+    y2Loess = loess(x2, y2, sigma, kernel = kernel)
     
     return np.concatenate([y1Loess, y2Loess], axis = 0)
